6ade850b6111c968ff22e67ff02046b75f98cc24
Andrii Kozubenko <Andrii.Kozubenko.IT.2021@lpnu.ua>
2022-09-13 13:57:25 +0300

    Add project files.

---
+++ CryptocurrencyBrowser/Models/CryptoCurrency.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptocurrencyBrowser.Models
+{
+    public class CryptoCurrency
+    {
+        public string Id { get; }
+        public string Name { get; }
+        public decimal Price { get; }
+        public decimal VolumeUsd24h { get; }
+        public double ChangePercent24h { get; }
+            
+        public CryptoCurrency(string id, string name, decimal price, decimal volumeUsd24h, double changePercent24h)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            VolumeUsd24h = volumeUsd24h;
+            ChangePercent24h = changePercent24h;
+        }
+    }
+}
---
+++ CryptocurrencyBrowser/Models/CryptoCurrencyList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace CryptocurrencyBrowser.Models
+{
+    public class CryptoCurrencyList
+    {
+        public CryptoCurrency[] cryptoCurrencies { get; set; } = Array.Empty<CryptoCurrency>();
+
+        private static readonly string _coinRequestUrl = "https://api.coincap.io/v2/assets?limit=10";
+
+        public static async Task<CryptoCurrencyList> GetTopTenCurrency()
+        {
+            var httpClient = new HttpClient();
+
+            var requestResult = await httpClient.GetStringAsync(_coinRequestUrl);
+
+            var result = JsonConvert.DeserializeObject<CryptoCurrencyList>(requestResult);
+
+            return result ?? new CryptoCurrencyList();
+        }
+    }
+}
